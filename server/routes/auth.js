b2400d7bdf5aaef3d96e1b9e57a5e74298423ebe
vaibhav290105 <130433399+vaibhav290105@users.noreply.github.com>
2025-08-02 22:05:40 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -113,8 +113,8 @@ const transporter = nodemailer.createTransport({
   port: process.env.SMTP_PORT || 587,
   secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: "vaibhavgupta290105@gmail.com",
+    pass: wsql tcfy xvun nmxy,
   },
 });
 
